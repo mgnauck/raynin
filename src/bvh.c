cc2e291d5f7a6019a8c830d9c7f016840bbd366d
Markus Gnauck <m.gnauck@xceptance.com>
2024-01-01 20:51:38 +0100

    Bvh creation (WIP)

--- src/bvh.c
+++ src/bvh.c
@@ -2,9 +2,15 @@
 #include <float.h>
 #include "scn.h"
 #include "aabb.h"
+#include "mutil.h"
 #include "log.h"
 
-#define NODE_INTERVAL_COUNT 8
+#define INTERVAL_CNT 8
+
+typedef struct interval {
+  aabb    aabb;
+  size_t  cnt;
+} interval;
 
 typedef struct split {
   float   cost;
@@ -15,9 +21,66 @@ typedef struct split {
 split find_best_cost_interval_split(const scn *s, bvh_node *n)
 {
   split best = { .cost = FLT_MAX };
-
   for(uint8_t axis=0; axis<3; axis++) {
-   // TODO 
+    // Calculate bounds of object centers
+    float minc = FLT_MAX;
+    float maxc = -FLT_MAX;
+    for(size_t i=0; i<n->obj_cnt; i++) {
+      float c = scn_get_obj_center(s, n->start_idx + i).v[axis];
+      minc = min(minc, c);
+      maxc = max(maxc, c);
+    }
+    if(fabsf(maxc - minc) < EPSILON)
+      continue;
+    
+    // Initialize empty intervals
+    interval intervals[INTERVAL_CNT];
+    for(size_t i=0; i<INTERVAL_CNT; i++)
+      intervals[i] = (interval){ aabb_init(), 0 };
+
+    // Count objects per interval and find their combined bounds
+    float delta = INTERVAL_CNT / (maxc - minc);
+    for(size_t i=0; i<n->obj_cnt; i++) {
+      vec3 center = scn_get_obj_center(s, n->start_idx + i);
+      uint32_t int_idx =
+        (uint32_t)min(INTERVAL_CNT - 1, (center.v[axis] - minc) / delta);
+      intervals[int_idx].aabb = aabb_combine(
+          intervals[int_idx].aabb, scn_get_obj_aabb(s, n->start_idx + i));
+      intervals[int_idx].cnt++;
+    }
+
+    // Calculate left/right area and count for each plane separating the intervals
+    float areas_l[INTERVAL_CNT - 1];
+    float areas_r[INTERVAL_CNT - 1];
+    size_t cnts_l[INTERVAL_CNT - 1];
+    size_t cnts_r[INTERVAL_CNT - 1];
+    aabb aabb_l = aabb_init();
+    aabb aabb_r = aabb_init();
+    size_t total_cnt_l = 0;
+    size_t total_cnt_r = 0;
+    for(size_t i=0; i<INTERVAL_CNT - 1; i++) {
+      // From left
+      total_cnt_l += intervals[i].cnt;
+      cnts_l[i] = total_cnt_l;
+      aabb_l = aabb_combine(aabb_l, intervals[i].aabb);
+      areas_l[i] = aabb_calc_area(aabb_l);
+      // From right
+      total_cnt_r += intervals[INTERVAL_CNT - 1 - i].cnt;
+      cnts_r[INTERVAL_CNT - 2 - i] = total_cnt_r;
+      aabb_r = aabb_combine(aabb_r, intervals[INTERVAL_CNT - 1 - i].aabb);
+      areas_r[INTERVAL_CNT - 2 - i] = aabb_calc_area(aabb_r);
+    }
+
+    // Find best surface area cost for prepared interval planes
+    delta = (maxc - minc) / (float)INTERVAL_CNT;
+    for(size_t i=0; i<INTERVAL_CNT - 1; i++) {
+      float cost = cnts_l[i] * areas_l[i] + cnts_r[i] * areas_r[i];
+      if(cost < best.cost) {
+        best.cost = cost;
+        best.axis = axis;
+        best.pos = minc + (i + 1) * delta;
+      }
+    }
   }
 
   return best;
@@ -39,9 +102,10 @@ bvh_node *bvh_add_node(bvh *b, const scn *s, int32_t start_idx, size_t obj_cnt)
       node_aabb.max.x, node_aabb.max.y, node_aabb.max.z);
   log("start_idx: %d, obj_cnt: %d", start_idx, obj_cnt);
   log("---");
-
-  bvh_node n = { node_aabb.min, start_idx, node_aabb.max, obj_cnt };
-  return memcpy(&b->node_buf[b->node_cnt++], &n, sizeof(n));
+ 
+  return memcpy(&b->node_buf[b->node_cnt++],
+      &(bvh_node){ node_aabb.min, start_idx, node_aabb.max, obj_cnt },
+      sizeof(bvh_node));
   //return &b->node_buf[b->node_cnt - 1];
 }
 
